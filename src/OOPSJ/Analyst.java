69cb37b8e7795d1d415759b5fdc0a6a409e3dc1e
Makhal.W <baruur.co@gmail.com>
2022-03-03 14:44:12 -0600

    Initial commit

---
+++ src/OOPSJ/Analyst.java
@@ -0,0 +1,15 @@
+package OOPSJ;
+
+public class Analyst extends Employee {
+
+    public Analyst(String name, double salary, int age)
+    {
+        super(name, salary, age);
+    }
+    public double getAnnualBonus()
+    {
+        return this.salary * .05;
+    }
+
+}
+
